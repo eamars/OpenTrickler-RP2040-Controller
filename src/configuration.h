aa6335be0a3a25378c0555e81f26b9b7b30465b4
Ran Bao <worksev@gmail.com>
2025-02-26 00:03:38 +1300

    remove pico configuration as not needed

--- src/configuration.h
+++ src/configuration.h
@@ -3,14 +3,7 @@
 
 #include "hardware/spi.h"
 #include "hardware/uart.h"
-
-#ifdef RASPBERRYPI_PICO
-#include "raspberrypi_pico_config.h"
-#endif
-
-#ifdef RASPBERRYPI_PICO_W
 #include "raspberrypi_pico_w_config.h"
-#endif
 
 
 #endif  // CONFIGURATION_H_
